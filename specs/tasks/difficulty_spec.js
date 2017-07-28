4d4cea5a5e5294f0c1a1b2964decebb60221dbaa
James Cox <j-cox03@hotmail.com>
2017-07-28 11:18:17 +0100

    Made tests for other levels of difficulty

--- specs/tasks/difficulty_spec.js
+++ specs/tasks/difficulty_spec.js
@@ -3,9 +3,29 @@ var difficulty = require("../../tasks/difficulty.js");
 
 describe("difficulty", function(){
 
-  it("Should be able to get the first levels of difficulty", function(){
+  it("Should be able to get the first level of difficulty", function(){
     assert.strictEqual(difficulty.basic.type, "difficulty");
     assert.strictEqual(difficulty.basic.value, 0);
+  });
+
+  it("Should be able to get the second level of difficulty", function(){
+    assert.strictEqual(difficulty.easy.type, "difficulty");
+    assert.strictEqual(difficulty.easy.value, 1);
+  });
+
+  it("Should be able to get the third level of difficulty", function(){
+    assert.strictEqual(difficulty.normal.type, "difficulty");
+    assert.strictEqual(difficulty.normal.value, 2);
+  });
+
+  it("Should be able to get the fourth level of difficulty", function(){
+    assert.strictEqual(difficulty.hard.type, "difficulty");
+    assert.strictEqual(difficulty.hard.value, 3);
+  });
+
+  it("Should be able to get the fifth level of difficulty", function(){
+    assert.strictEqual(difficulty.extreme.type, "difficulty");
+    assert.strictEqual(difficulty.extreme.value, 4);
   });
 
 });
