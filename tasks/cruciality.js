b1177bb704f3a88e2c73179ec7a30a82508660ac
James Cox <j-cox03@hotmail.com>
2017-07-28 11:29:42 +0100

    Cruciality: made method greater than

--- tasks/cruciality.js
+++ tasks/cruciality.js
@@ -4,4 +4,11 @@ function Cruciality(type, value){
   this.value = value;
 }
 
+Cruciality.prototype.greaterThan = function(toCompare){
+  if(this.type != toCompare.type){
+    throw "The two values are not of the same type"
+  }
+  return this.value > toCompare.value;
+}
+
 module.exports = Cruciality;
