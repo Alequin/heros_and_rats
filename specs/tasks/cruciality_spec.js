ff0147e90c253bffc5b39ac7adc37573d5222ea6
James Cox <j-cox03@hotmail.com>
2017-07-28 11:44:47 +0100

    cruciality_spec: made tests for less than method

--- specs/tasks/cruciality_spec.js
+++ specs/tasks/cruciality_spec.js
@@ -16,4 +16,17 @@ describe("cruciality", function(){
     assert.throws(function(){ val1.greaterThan(val2) }, Error, "Cannot compare values of different types");
   });
 
+  it("can check if one is less than another (when type value is equal)", function(){
+    var val1 = new Cruciality("type1", 0);
+    var val2 = new Cruciality("type1", 1);
+    assert.strictEqual(val1.lessThan(val2), true);
+    assert.strictEqual(val2.lessThan(val1), false);
+  });
+
+  it("cannot check less than status when types are different", function(){
+    var val1 = new Cruciality("type1", 0);
+    var val2 = new Cruciality("type2", 1);
+    assert.throws(function(){ val1.lessThan(val2) }, Error, "Cannot compare values of different types");
+  });
+
 });
