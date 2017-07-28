de173688a4defde1cec4b7ade65edc37fca3bd91
James Cox <j-cox03@hotmail.com>
2017-07-28 11:25:04 +0100

    cruciality_spec: made test for comparing two objects of the same type

--- specs/tasks/cruciality_spec.js
+++ specs/tasks/cruciality_spec.js
@@ -1,5 +1,13 @@
 var assert = require("assert");
+var Cruciality = require("../../tasks/cruciality.js");
 
 describe("cruciality", function(){
+
+  it("can check if one is greater than another (when type value is equal)", function(){
+    var val1 = new Cruciality("type1", 0);
+    var val2 = new Cruciality("type1", 1);
+    assert.strictEqual(val1.greaterThan(val2), false);
+    assert.strictEqual(val2.greaterThan(val1), true);
+  });
 
 });
