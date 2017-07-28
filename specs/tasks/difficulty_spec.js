734f393c04b0012d73023b9bce858b1bf9ea6f05
James Cox <j-cox03@hotmail.com>
2017-07-28 11:13:06 +0100

    difficulty_spec: added test for getting first level of difficulty

--- specs/tasks/difficulty_spec.js
+++ specs/tasks/difficulty_spec.js
@@ -1,5 +1,11 @@
 var assert = require("assert");
+var difficulty = require("../../tasks/difficulty.js");
 
 describe("difficulty", function(){
+
+  it("Should be able to get the first levels of difficulty", function(){
+    assert.strictEqaul(difficulty.basic.type, "difficulty");
+    assert.strictEqaul(difficulty.basic.value, 0);
+  });
 
 });
