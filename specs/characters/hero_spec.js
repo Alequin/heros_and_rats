36a440560ce88808a0903f69545db19006d259a4
James Cox <j-cox03@hotmail.com>
2017-07-28 13:41:27 +0100

    hero_spec: required hero to have max health

--- specs/characters/hero_spec.js
+++ specs/characters/hero_spec.js
@@ -12,6 +12,7 @@ describe("hero", function(){
   it("on costruction has name, health and favourite food set and tasks and money empty", function(){
     assert.strictEqual(hero1.name, "Taako");
     assert.strictEqual(hero1.health, 50);
+    assert.strictEqual(hero1.maxHealth, 50);
     assert.strictEqual(hero1.favouriteFood, "Ferrero Rocher");
     assert.strictEqual(hero1.money, 0);
     assert.deepEqual(hero1.tasks, []);
@@ -21,6 +22,6 @@ describe("hero", function(){
     assert.strictEqual(hero1.sayName(), "Hail and well met my dudes. Im Taako");
   });
 
-  
+
 
 });
