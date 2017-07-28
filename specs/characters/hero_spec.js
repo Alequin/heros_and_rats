1bd723de2827bd185ac76cefece8b2ee08940572
James Cox <j-cox03@hotmail.com>
2017-07-28 14:03:15 +0100

    hero_spec: made test for eating poisoned food

--- specs/characters/hero_spec.js
+++ specs/characters/hero_spec.js
@@ -5,9 +5,11 @@ var Food = require("../../modifier/food.js");
 describe("hero", function(){
 
   var hero1;
+  var food1;
 
   beforeEach(function(){
     hero1 = new Hero("Taako", 50, "Ferrero Rocher");
+    food1 = new Food("macaroon", 5, false);
   });
 
   it("on costruction has name, health and favourite food set and tasks and money empty", function(){
@@ -25,17 +27,22 @@ describe("hero", function(){
 
   it("can eat to gain health", function(){
     var startHealth = hero1.health;
-    var food = new Food("macaroon", 5, false);
     hero1.health -= 5;
-    hero1.eat(food);
+    hero1.eat(food1);
     assert.strictEqual(hero1.health, startHealth);
   });
 
   it("cannot eat to gain health more than max health", function(){
     var startHealth = hero1.health;
-    var food = new Food("macaroon", 5, false);
-    hero1.eat(food);
+    hero1.eat(food1);
     assert.strictEqual(hero1.health, startHealth);
+  });
+
+  it("will loose health if food is poisoned", function(){
+    var startHealth = hero1.health;
+    food1.isPoisoned = true;
+    hero1.eat(food1);
+    assert.strictEqual(hero1.health, startHealth-5);
   });
 
 
