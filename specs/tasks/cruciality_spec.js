679e05199de54bf5860394a493f53edd07315293
James Cox <j-cox03@hotmail.com>
2017-07-28 11:58:51 +0100

    cruciality_spec: made test to see if values are equal

--- specs/tasks/cruciality_spec.js
+++ specs/tasks/cruciality_spec.js
@@ -29,4 +29,18 @@ describe("cruciality", function(){
     assert.throws(function(){ val1.lessThan(val2) }, Error, "Cannot compare values of different types");
   });
 
+  it("can check if one is equal to another (when type value is equal)", function(){
+    var val1 = new Cruciality("type1", 0);
+    var val2 = new Cruciality("type1", 0);
+    var val3 = new Cruciality("type1", 1);
+    assert.strictEqual(val1.equalTo(val2), true);
+    assert.strictEqual(val1.equalTo(val3), false);
+  });
+
+  it("cannot check equality status when types are different", function(){
+    var val1 = new Cruciality("type1", 0);
+    var val2 = new Cruciality("type2", 0);
+    assert.throws(function(){ val1.equalTo(val2) }, Error, "Cannot compare values of different types");
+  });
+
 });
