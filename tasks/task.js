cee9e005a0e52f0270ec44552a944e78d8ac352f
James Cox <j-cox03@hotmail.com>
2017-07-28 13:01:25 +0100

    Task: throw errors in constructor when types are mis-matched

--- tasks/task.js
+++ tasks/task.js
@@ -1,6 +1,13 @@
 
 function Task(difficulty, urgency, reward){
 
+  if(difficulty.type != "difficulty"){
+    throw new Error("Difficulty must be of type difficulty. Current: " + difficulty.type);
+  }
+  if(urgency.type != "urgency"){
+    throw new Error("Difficulty must be of type difficulty. Current: " + urgency.type);
+  }
+
   this.difficulty = difficulty;
   this.urgency = urgency;
   this.reward = reward;
