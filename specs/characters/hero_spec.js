3d99f6eb2c0c7d4815cc22df6c43b4090ccd770b
James Cox <j-cox03@hotmail.com>
2017-07-28 13:36:43 +0100

    hero_spec: made beforeEach

--- specs/characters/hero_spec.js
+++ specs/characters/hero_spec.js
@@ -3,13 +3,22 @@ var Hero = require("../../characters/hero.js");
 
 describe("hero", function(){
 
+  var hero1;
+
+  beforeEach(function(){
+    hero1 = new Hero("Taako", 50, "Ferrero Rocher");
+  });
+
   it("on costruction has name, health and favourite food set and tasks and money empty", function(){
-    var hero = new Hero("Taako", 50, "Ferrero Rocher");
-    assert.strictEqual(hero.name, "Taako");
-    assert.strictEqual(hero.health, 50);
-    assert.strictEqual(hero.favouriteFood, "Ferrero Rocher");
-    assert.strictEqual(hero.money, 0);
-    assert.deepEqual(hero.tasks, []);
+    assert.strictEqual(hero1.name, "Taako");
+    assert.strictEqual(hero1.health, 50);
+    assert.strictEqual(hero1.favouriteFood, "Ferrero Rocher");
+    assert.strictEqual(hero1.money, 0);
+    assert.deepEqual(hero1.tasks, []);
+  });
+
+  it("can say name", function(){
+
   });
 
 });
