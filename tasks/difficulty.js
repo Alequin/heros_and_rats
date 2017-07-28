4146675044df6a63c14a72a1c4c19b5640003247
James Cox <j-cox03@hotmail.com>
2017-07-28 11:19:18 +0100

    Made the other four levels of diffculty

--- tasks/difficulty.js
+++ tasks/difficulty.js
@@ -4,6 +4,14 @@ var difficulty = {
 
   basic: new Cruciality("difficulty", 0),
 
+  easy: new Cruciality("difficulty", 1),
+
+  normal: new Cruciality("difficulty", 2),
+
+  hard: new Cruciality("difficulty", 3),
+
+  extreme: new Cruciality("difficulty", 4),
+
 }
 
 module.exports = difficulty;
