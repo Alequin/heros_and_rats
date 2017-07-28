7640dbba6fbeed1be4ff28dcef4e5edef5990c99
James Cox <j-cox03@hotmail.com>
2017-07-28 12:32:24 +0100

    food_spec: made test for constructing food

--- specs/modifier/food_spec.js
+++ specs/modifier/food_spec.js
@@ -1,5 +1,13 @@
 var assert = require("assert");
+var Food = require("../../modifier/food.js");
 
 describe("food", function(){
+
+  it("on construction can set name, modifier and poisoned status", function(){
+    var food1 = new Food("macaroon", 5, false);
+    assert.strictEqual(food1.name, "macaroon");
+    assert.strictEqual(food1.modifier, 5);
+    assert.strictEqual(food1.poisoned, false);
+  });
 
 });
