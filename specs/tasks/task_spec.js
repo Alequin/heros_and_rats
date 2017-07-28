a89b296b1e7785cfec4a2cde289550cf496763b2
James Cox <j-cox03@hotmail.com>
2017-07-28 12:51:12 +0100

    task_spec: made construction test

--- specs/tasks/task_spec.js
+++ specs/tasks/task_spec.js
@@ -1,5 +1,17 @@
 var assert = require("assert");
+var difficulty = difficulty = require("../../tasks/difficulty.js");
+var urgency = require("../../tasks/urgency.js");
+var Task = require("../../tasks/task.js");
 
 describe("task", function(){
+
+
+  it("should have a difficulty, urgency, reward and a false complete status on construction", function(){
+    var task = new Task(difficulty.hard, urgency.medium, 100);
+    assert.strictEqual(task.difficulty, difficulty.hard);
+    assert.strictEqual(task.urgency, urgency.medium);
+    assert.strictEqual(task.reward, 100);
+    assert.strictEqual(task.isComplete, false);
+  });
 
 });
