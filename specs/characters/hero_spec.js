820e802c158ec4cff25de4d6977f3e62fd986673
James Cox <j-cox03@hotmail.com>
2017-07-28 13:28:51 +0100

    hero_spec: made tests for hero construction

--- specs/characters/hero_spec.js
+++ specs/characters/hero_spec.js
@@ -1,5 +1,15 @@
 var assert = require("assert");
+var Hero = require("../../characters/hero.js");
 
 describe("hero", function(){
+
+  it("on costruction has name, health and favourite food set and tasks and money empty", function(){
+    var hero = new Hero("Taako", 50, "Ferrero Rocher");
+    assert.strictEqual(hero.name, "Taako");
+    assert.strictEqual(hero.health, 50);
+    assert.strictEqual(hero.favouriteFood, "Ferrero Rocher");
+    assert.strictEqual(hero.money, 0);
+    assert.deepEqual(hero.tasks, []);
+  });
 
 });
