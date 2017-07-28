30d5c1bef2234a2f4f406c10450b5ef24cdc100d
James Cox <j-cox03@hotmail.com>
2017-07-28 12:42:28 +0100

    Rat_spec: made test for rat touching food

--- specs/characters/rat_spec.js
+++ specs/characters/rat_spec.js
@@ -1,5 +1,16 @@
 var assert = require("assert");
+var Food = require("../../modifier/food.js");
+var Rat = require("../../characters/rat.js");
 
 describe("rat", function(){
+
+  it("can poison food", function(){
+    var food = new Food("macaroon", 5, false);
+    var rat = new Rat();
+
+    assert.strictEqual(food.isPoisoned, false);
+    rat.touchFood(food);
+    assert.strictEqual(food.isPoisoned, true);
+  });
 
 });
