6313430aac6d25b20e78543ff9830a488dfa5e17
James Cox <j-cox03@hotmail.com>
2017-07-28 11:57:19 +0100

    Cruciality: refactored greater than and less than methods

--- tasks/cruciality.js
+++ tasks/cruciality.js
@@ -5,17 +5,27 @@ function Cruciality(type, value){
 }
 
 Cruciality.prototype.greaterThan = function(toCompare){
-  if(this.type != toCompare.type){
-    throw new Error("Cannot compare values of different types");
-  }
-  return this.value > toCompare.value;
+  return this.compare(toCompare) > 0;
 }
 
 Cruciality.prototype.lessThan = function(toCompare){
+  return this.compare(toCompare) < 0;
+}
+
+
+Cruciality.prototype.compare = function(toCompare){
   if(this.type != toCompare.type){
     throw new Error("Cannot compare values of different types");
   }
-  return this.value < toCompare.value;
+
+  if(this.value > toCompare.value){
+    return 1;
+  }else
+  if(this.value < toCompare.value){
+    return -1;
+  }else{
+    return 0;
+  }
 }
 
 module.exports = Cruciality;
